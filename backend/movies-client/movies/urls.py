7932a401b7e557d8ad45504bf55a236f2681bf9d
JoseDiaz24-p <jose.diaz.orellana@hotmail.com>
2026-09-17 09:15:23 -0300

    Elimina proyectos backend

--- backend/movies-client/movies/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import MovieListView, MovieDetailView
-
-urlpatterns = [
-    path('movies/', MovieListView.as_view(), name='movie-list'),
-    path('movies/<int:tmdb_id>/', MovieDetailView.as_view(), name='movie-detail'),
-]
